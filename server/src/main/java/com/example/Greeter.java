e0165147f3b5765a942ec4e9483c381529ae99ed
sam <470289074@qq.com>
2019-06-02 10:23:42 +0900

    init

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,23 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  /**
+   * Javadoc comment.
+   * @param someone the name of a person
+   * @return greet string
+   */
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
